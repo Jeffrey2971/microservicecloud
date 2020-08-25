22bbfdf030cf9d471d062ff9ec04d6ecd95d4498
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-08-26 00:14:58 +0800

    SpringCloud

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/jeffrey/springcloud/dao/DeptDao.java
@@ -0,0 +1,30 @@
+package com.jeffrey.springcloud.dao;
+
+
+import com.jeffrey.springcloud.entitys.Dept;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DeptDao {
+
+    /**
+     * 添加部门
+     * @return
+     */
+    public boolean addDept();
+
+    /**
+     * 查找一个用户
+     * @param id
+     * @return
+     */
+    public Dept findById(Long id);
+
+    /**
+     * 查找部门
+     * @return
+     */
+    public List<Dept> findAll();
+}
