7d705902f204a82848f8497f8332ab9c8c70588f
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-08-28 01:01:42 +0800

    SpringCloud

--- microservicecloud-provider-dept-8001/src/main/java/com/jeffrey/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/jeffrey/springcloud/controller/DeptController.java
@@ -3,12 +3,17 @@ package com.jeffrey.springcloud.controller;
 import com.jeffrey.springcloud.entitys.Dept;
 import com.jeffrey.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class DeptController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Autowired
     private DeptService service;
@@ -28,5 +33,18 @@ public class DeptController {
     @GetMapping(value = "/dept/list")
     public List<Dept> list() {
         return service.list();
+    }
+
+    @GetMapping(value = "/dept/discovery")
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.discoveryClient;
     }
 }
