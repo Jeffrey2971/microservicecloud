6fbafa4fc6f499931a4cccac2b37a3d3cd952d9f
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-08-27 15:26:17 +0800

    SpringCloud

--- microservicecloud-provider-dept-8001/src/main/java/com/DeptProvider8001_APP.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/DeptProvider8001_APP.java
@@ -2,7 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+// 表示本服务启动后将会自动注册进 eureka 注册中心中
+@EnableEurekaClient
 @SpringBootApplication
 public class DeptProvider8001_APP {
     public static void main(String[] args) {
